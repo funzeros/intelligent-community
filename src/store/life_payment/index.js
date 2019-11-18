ff0ca69a91e725e1a56c0f1e2362b1738ded093d
Get-zpl <2242684417@qq.com>
2019-11-18 20:45:15 +0800

    缴费渲染

--- src/store/life_payment/index.js
+++ src/store/life_payment/index.js
@@ -1,69 +1,41 @@
-// 生活缴费
+// 生活缴费---周佩蕾
 import axios from 'axios';
 export default {
-    namespaced: true,
-    state: {
-        userpayments: [
-            {
-              id: "110",
-              homenumber: "1幢1单元1201室",
-              paystyle: "电费",
-              price: "35.64",
-              money:'30',
-              ordernumber: "2019102900001",
-              date: "2016/02/01-2016/03/01",
-              premonth: "3387.0",
-              nowmonth: "3432.0"
-            },
-            {
-              id: "111",
-              homenumber: "1幢1单元1201室",
-              paystyle: "水费",
-              price: "46.84",
-              money:'30',
-              ordernumber: "2019102900001",
-              date: "2016/02/01-2016/03/01",
-              premonth: "3387.0",
-              nowmonth: "3432.0"
-            },
-             {
-              id: "112",
-              homenumber: "1幢1单元1201室",
-              paystyle: "水费",
-              price: "30.00",
-              money:'30',
-              ordernumber: "2019102900001",
-              date: "2016/02/01-2016/03/01",
-              premonth: "3387.0",
-              nowmonth: "3432.0"
-            }, 
-         
-          ],
-        paymentone:[]
+  namespaced: true,
+  state: {
+    userpayments: [],
+    paymentone: []
+  },
+  mutations: {
+
+    setNowPay(state, paynow) {
+      state.paymentone = paynow;
     },
-    mutations: {
-        //修改数据 的方法 返回promise对象  async（function（）） awat 等待结果
-        setNowPay(state, paynow) {
-            state.paymentone = paynow;
-        }
+    setUserPay(state, paylist) {
+      state.userpayments = paylist;
+    }
+  },
+  actions: {
+
+    // 获取所有的未缴费数据
+    async justpayment({ commit, state }, paymentId) {
+   
+      const result = await axios.get('/api/user/life?uId=13&sStatus=0');
+      const userpaylist=result.data.data;
+      commit('setUserPay',userpaylist);
+      console.log(state.userpayments)
+
+  
     },
-    actions: {
-        
-       // 接收 事件方法 可以带参数
-       async justpayment({commit,state}, paymentId) {
-        //    console.log( paymentId);
-        //    console.log( state.userpayments.length)
-        //    console.log( state.paymentone)
-
-        for(let i=0;i<state.userpayments.length;i++){
-            if(state.userpayments[i].id===paymentId){
-                state.paymentone=state.userpayments[i];
-            }
-        }
-        commit('setNowPay', state.paymentone)
-        //console.log( state.paymentone)
-    }
+    // 对应ID的详情数据
+   async detailpay({ commit, state }, detailId){
+    
+        const datailinfo = await axios.get('/api/user/life?uId=13&sStatus=0');
+        const dataillist=datailinfo.data.data[detailId];
+        commit('setNowPay',dataillist);
+    console.log(dataillist)
+   }
 
 
-    }
+  }
 }
