fe1b6dcbda930b415c89349ada4bd7cea741017d
weizhenjun123 <1397358097@qq.com>
2019-11-18 21:49:05 +0800

    暂时

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,21 @@ module.exports = {
                 target: 'http://106.13.93.13:9081',
                 ws: true,
                 changeOrigin: true
-            }
+            },
+            // 投诉建议
+           '/myadvice':{
+               target:"http://106.13.93.13:9021",
+               ws:true,
+               changeOrigin:true
+           },
+        //    回复接口
+           '/response':{
+                target:"http://106.13.93.13:9021/",
+                ws:true,
+                changeOrigin:true
+            },
+
+           
         }
-    },
-};
+}
+}
